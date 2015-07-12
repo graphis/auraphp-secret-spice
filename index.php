195853f2fbc6c371e87b7ab6871f9b5af039c881
Zsolt Sándor <ezsolt@gmail.com>
2015-07-12 13:15:16 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 
+ * This file is part of my_application.
+ * 
+ * @package my_application
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
+
+$path = dirname(__DIR__);
+require "{$path}/vendor/autoload.php";
+
+
+
+
+
+
+
+
+
+// eof
