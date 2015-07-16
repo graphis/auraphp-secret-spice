266634ee6d28cb5ebe1306c1a74fe8c42b95e23a
Zsolt Sándor <ezsolt@gmail.com>
2015-07-16 02:41:44 +0200

    Update Micro.php

--- application/classes/Micro.php
+++ application/classes/Micro.php
@@ -1,22 +1,21 @@
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
 <?php
-/*
- * @package Aura.Micro
+/**
  *
- * @license http://opensource.org/licenses/MIT MIT
+ * This file is part of my_application.
+ * application/bootstrap.php is responsible to load application routes and classes, then handle all to application/classes/micro.class
+ *
+ * @package my_application
+ * @version    1.7
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * @copyright  2015 Zsolt Sándor
+ *
  */
+
 namespace Application;
 
 use Aura\Router\RouterFactory;
+
+
 
 /**
  *
@@ -30,45 +29,16 @@ use Aura\Router\RouterFactory;
 class Micro
 {
 
-    /**
-     * Request method GET
-     */
-    const METHOD_GET = "GET";
-
-    /**
-     * Request method POST
-     */
-    const METHOD_POST = "POST";
-
-    /**
-     * Request method PUT
-     */
-    const METHOD_PUT = "PUT";
-
-    /**
-     * Request method DELETE
-     */
-    const METHOD_DELETE = "DELETE";
-
-    /**
-     * Callback before routes are dispatched
-     */
-    const CALLBACK_BEFORE = 'before';
-
-    /**
-     * Callback after routes are dispatched
-     */
-    const CALLBACK_AFTER = 'after';
+   /**
+    * Routes file path
+    */
+    const ROUTES = APPPATH . 'routes.php';
 
     /**
      * Callback after routing and error handling
      */
     const CALLBACK_FINISH = 'finish';
 
-    /**
-     * Callback when a routing error occurs
-     */
-    const CALLBACK_ERROR = 'error';
 
     /**
      * Aura\Router\Map collection of routes
@@ -92,247 +62,124 @@ class Micro
 
 
 
-
-
-
-
-
-
-
-
     /**
      * Create a Micro framework application
      *
-     * @uses Aura\Router\Map
-     * @uses Aura\Router\DefinitionFactory
-     * @uses Aura\Router\RouteFactory
+     * @uses Aura\Router\RouterFactory;
      */
      public function __construct()
      {
-       echo '<br/> 00 construct <pre><code>';
-       $router_factory = new RouterFactory;
-       $this->router = $router_factory->newInstance();
+       echo '__construct<br/>';
+       $this->before();
 
-	      // get routes
-	      $this->get_Resource_File( APPPATH . 'routes.php');
-     // echo 'aaa';
-//       $this->router->add('test', '{test}')
-  //     	->addTokens(array(
-    //   		'test' => '/zorro', //   work OR play OR dream
-      // 	));
+       //
+       $this->router_factory = new RouterFactory;
+       $this->router = $this->router_factory->newInstance();
 
-       // $this->map = new Map(new DefinitionFactory, new RouteFactory);
+       // get the routes
+       $this->getRoutes();
+       $this->after();
      }
 
 
 
-     // get required file
-     public function get_Resource_File($file)
+     public function debug($data)
      {
-       echo getcwd();
-       echo '_____ ' . APPPATH . $file . '_____';
+       echo '<br/>';
 
-     	if (file_exists($file)) {
-     		// Let the app specify it's own routes.
-        include_once($file);
-     	} else {
-     	//	echo 'the required file '.$file.' was not found';
-     		// Fall back on some sensible defaults.
-     		// $router->add(null, '/');
-     		// $router->add(null, '/{controller}');
-     		// $router->add(null, '/{controller}/{action}');
-     		// $router->add(null, '/{controller}/{action}/{id}');
-     	}
+       echo '<pre><code>';
+       print_r($data);
+       echo '</code></pre>';
 
-     //	return include_once( '../application/'.$file);
+       echo '<br/>';
      }
-
 
 
 
     /**
-     * Get the app router object so it can be worked with directly
+     * build routes
      *
      * @return Aura\Router\Map
      */
-    public function getMap()
+    public function getRoutes()
     {
-        return $this->map;
-        // zorro
+      echo 'getRoutes<br/>';
+
+      // Routes are defined in app / routes.php
+      // $file = APPPATH . 'routes.php';
+
+      if (file_exists(self::ROUTES)) {
+        // Let the app specify it's own routes.
+        include_once(self::ROUTES);
+      } else {
+        //	echo 'the required file '.$file.' was not found';
+        // Fall back on some sensible defaults.
+        // $router->add(null, '/');
+        // $router->add(null, '/{controller}');
+        // $router->add(null, '/{controller}/{action}');
+        // $router->add(null, '/{controller}/{action}/{id}');
+      }
+      //	return include_once( '../application/'.$file);
     }
 
-  /**
-   * Set a method parameter
-   *
-   * @param string $key Name of parameter
-   * @param mixed $value Value of parameter to be passed
-   */
-  public function set($key, $value)
-  {
-    if (array_key_exists($key, $this->params)) {
-      throw new \InvalidArgumentException(sprintf("Paramter %s has already been defined", $key));
-    }
 
-    $this->params[$key] = $value;
 
-    return $this;
-  }
-
-  /**
-   * Fetches a method parameter
-   *
-   * @param string $key Name of parameter
-   * @return mixed The result of the parameter
-   */
-  public function fetch($key)
-  {
-    if (!array_key_exists($key, $this->params)) {
-      return null; // Die quietly
-    }
-
-    if ($this->params[$key] instanceof \Closure) {
-      $this->params[$key] = $this->params[$key]();
-    }
-
-    return $this->params[$key];
-  }
-
-    /**
-     * Raw route -> controller add function
-     *
-     * @param string $method GET/POST/PUT/TYPE http request method
-     *
-     * @param string $route  Route to be matched against
-     *
-     * @param closure $controller Closure of the controller to be executed
-     *
-     * @return object
-     */
-    public function add($method, $route, $controller)
+    // get required file
+    public function get_Resource_File($file)
     {
-      /*
-        $this->map->add(
-            null,
-            $route,
-            [
-                'method' => $method,
-                'values' => [
-                    'controller' => $controller,
-                ]
-            ]
-        );
-        */
-        return $this; // probably should return something else...
-    }
-
-    /**
-     * GET http request
-     *
-     * @param string  $route Route to be matched against
-     *
-     * @param closure $controller Closure of the controller to be executed
-     *
-     * @return instance of this
-     */
-    public function get($route, $controller)
-    {
-        return $this->add(self::METHOD_GET, $route, $controller);
-    }
-
-    /**
-     * POST http request
-     *
-     * @param string  $route Route to be matched against
-     *
-     * @param Closure $controller Closure of the controller to be executed
-     *
-     * @return instance of this
-     */
-    public function post($route, $controller)
-    {
-        return $this->add(self::METHOD_POST, $route, $controller);
-    }
-
-    /**
-     * PUT http request
-     *
-     * @param string $route Route to be matched against
-     *
-     * @param Closure $controller Closure of the controller to be executed
-     *
-     * @return instance of this
-     */
-    public function put($route, $controller)
-    {
-        return $this->add(self::METHOD_PUT, $route, $controller);
-    }
-
-    /**
-     * DELETE http request
-     *
-     * @param string $route Route to be matched against
-     *
-     * @param Closure $controller Closure of the controller to be executed
-     *
-     * @return instance of this
-     */
-    public function delete($route, $controller)
-    {
-        return $this->add(self::METHOD_DELETE, $route, $controller);
+      if (file_exists($file)) {
+        include_once($file);
+      } else {
+        //	echo 'the required file '.$file.' was not found';
+      }
+      //	return include_once( '../application/'.$file);
     }
 
     /**
      * Add callback for before routing dispatches
-     *
      * @param Closure $callback Closure Callback to be executed
-     *
      * @return void
      */
-    public function before($callback)
+    public function before()
     {
-        $this->callbacks[self::CALLBACK_BEFORE][] = $callback;
+        echo 'before<br/>';
     }
 
     /**
      * Add callback for after routing dispatches
-     *
      * @param Closure $callback Closure Callback to be executed
-     *
      * @return void
-     *
      */
-    public function after($callback)
+    public function after()
     {
-        $this->callbacks[self::CALLBACK_AFTER][] = $callback;
+      echo 'after<br/>';
     }
 
     /**
      * Add callback for when routing dispatching is finsihed
-     *
      * @param Closure $callback Closure Callback to be executed
-     *
      * @return void
-     *
      */
-    public function finish($callback)
+    public function finish()
     {
-        $this->callbacks[self::CALLBACK_FINISH][] = $callback;
+      echo 'finish<br/>';
     }
 
     /**
-     * Add callback for when there is an error in routing dispatch
-     *
+     * route not found -- 404 error
      * @param Closure $callback Closure Callback to be executed
-     *
      * @return void
      */
-    public function error($callback)
+    public function error($code)
     {
-        $this->callbacks[self::CALLBACK_ERROR][] = $callback;
+      // handle to a function
+      // no route found --- error 404
+      http_response_code($code);
+      $messages = array('Aw, crap!', 'Bloody Hell!', 'Uh Oh!', 'Nope, not here.', 'Huh?');
+      $title = $messages[array_rand($messages)];
+      echo $title . ' Sorry, page is not there -- '. $code;
+      exit();
     }
-
-
-
 
 
 
@@ -357,90 +204,83 @@ class Micro
 
 
 
-
-
-
-
-
-
-
     /**
-     * Run the application executing the dispatch process
+     * Run the application:
+     * Check routes an execute the dispatch process
      *
      * @return void
      */
     public function run($path = null, $request = null)
     {
-        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $route = $this->router->match($path, $_SERVER);
+      echo 'run<br/>';
 
+        // get the incoming request URL path
+        $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        // $path = rtrim($path, '/');
 
-        try {
-            if (is_null($path)) {
-              $dir = dirname($_SERVER['PHP_SELF']);
+// way 1        // get the route based on the url and server
+        $route = $this->router->match($url, $_SERVER);
 
-              echo '<br/> 01 is_null';
-
-              $path = (strlen($dir) > 1) ?
-              substr($_SERVER['REQUEST_URI'], strlen($dir)) :
-              $_SERVER['REQUEST_URI'];
-            }
-
-            // echo '<br/> lalala run';
-
-            if (is_null($request)) {
-              $request = $_SERVER;
-                echo '<br/> 02 $request is_null<br/>';
-            }
-
-            if ($request) {
-              echo '<br/> zzz route -- from micro class<br/>';
-            } else {
-              echo '<br/> zzz NO route -- from micro class<br/>';
-            }
-            // zzz ok till here
-
-//            if (false === ($route = $this->map->match($path, $request))) {
-              if (false === $route) {
-                throw new \InvalidArgumentException("No route found!");
-                echo '<br/> 03 is_null is it really?';
-            } else {
-
-              echo '<br/> 04 else';
-              //$params = $route->params;
-              print_r($route->params);
-
-                $params = $route->values;
-                print_r($params);
-
-                $controller = $params["controller"];
-                unset($params["controller"]);
-
-                $reflection = new \ReflectionFunction($controller);
-
-                $args = array();
-
-                foreach ($reflection->getParameters() as $parameter) {
-                    if (isset($params[$parameter->getName()])) {
-                        $args[] = $params[$parameter->getName()];
-                    } elseif (($value = $this->fetch($parameter->getName())) !== null) {
-            $args[] = $value;
-
-          } else {
-                        $args[] = null;
-                    }
-                }
-
-                $this->applyCallbacks(self::CALLBACK_BEFORE);
-
-                call_user_func_array($controller, $args);
-
-                $this->applyCallbacks(self::CALLBACK_AFTER);
-            }
-        } catch (\Exception $e) {
-            $this->applyCallbacks(self::CALLBACK_ERROR, $e);
+        if (!$route) {
+          echo 'no route !!';
+        } elseif ( isset($route->params['pages']) ) {
+          echo ' route  ok if isset !!';
         }
 
-        $this->applyCallbacks(self::CALLBACK_FINISH);
+
+
+
+
+
+
+// way 2
+        //
+        try {
+          // check for routes
+          if ($route) {
+
+              // there is a root, now get the params
+              $params = $route->params;
+
+              // does the route indicate an action?
+              if (isset($route->params['action'])) {
+                // echo '111111111' . $route->params['pages'];
+                  // take the action class directly from the route
+                  $staticpage = $params['pages'];
+                  echo $staticpage;
+                  // unset($params['pages']);
+
+                //  $action_class = $route->params['action'];
+              } else {
+                echo '2222222222';
+                  // use a default action class
+                  // $action_class = 'IndexAction';
+              }
+
+              // get the action value // test
+              // $action = $params['whatewer-is-the-action-name'];
+              // unset($params['action']);
+
+              $this->debug($params);
+              // echo '<br/> zzz route -- from micro class<br/>';
+          } else {
+              $this->debug($url);
+              $this->error('404');
+          }
+
+        } catch(\Exception $e) {
+            print_r($e);
+        }
+
+
+
+
+        //
+        $this->finish();
+
     }
 }
+
+
+
+// eof Micro.php
