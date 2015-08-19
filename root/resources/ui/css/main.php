0f65ae0ca75f3ce757d6da9b72f0733abffb4675
Zsolt Sándor <ezsolt@gmail.com>
2015-08-19 16:12:23 +0200

    serving css with php example

---
+++ root/resources/ui/css/main.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+//
+header("Content-type: text/css; charset: UTF-8");
+
+
+
+$brandColor = "#990000";
+$linkColor = "#555555";
+$CDNURL = "http://cdn.blahblah.net";
+
+
+
+
+
+?>
+
+/* css */
+a {
+  color: <?php echo $linkColor; ?>;
+}
+
+
+
