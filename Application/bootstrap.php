4c234d317a2abd1b99e9363194afc6b5d8716a3f
Zsolt Sándor <ezsolt@gmail.com>
2015-10-06 16:38:59 +0200

    minor update

--- Application/bootstrap.php
+++ Application/bootstrap.php
@@ -6,18 +6,15 @@
  *
  */
 
-
-
-// aura autoloader
+// using aura autoloader instead of composer autoloader -- for now
 use Aura\Autoload\Loader;
 
 // instantiate
 $loader = new \Aura\Autoload\Loader;
+
 // append to the SPL autoloader stack; use register(true) to prepend instead
 $loader->register();
 $loader->addPrefix('Application', APPPATH . 'classes');
-
-
 
 // kick the application in
 use Application\Application;
@@ -25,9 +22,4 @@ use Application\Application;
 $app = new Application();
 $app->run();
 
-
-
-
-
-
-// eof bootstrap.php
+//
